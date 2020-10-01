4e6b904476fdfa7ff6fc98adee85f0f9ce4f1e85
Michael Maurer <michaelmaurer@MacBook-Pro.local>
2020-10-01 17:23:16 -0400

    chore: switches to new example

--- App.js
+++ App.js
@@ -6,21 +6,46 @@
  * @flow strict-local
  */
 
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
 
-const HelloDnD = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Text>Hello, DnD World!</Text>
-    </View>
-  )
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Click me</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>You clicked {this.state.count} times</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
-export default HelloDnD
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10
+  }
+})
+
+export default App
