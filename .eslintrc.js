6af81d428f661ef697fdc4ad806a8017ba9fe0ef
Michael Maurer <michaelmaurer@MacBook-Pro.local>
2020-10-01 13:55:54 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'plugin:react-recommended',
+    'prettier/react',
+    'plugin:prettier-recommended',
+    'airbnb',
+  ],
+  "plugins": ["react", "prettier", "react-redux"],
+}
