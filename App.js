8cc7bfee05530722c2b65a1d8107a594e1b7193d
Michael Maurer <maumike87@gmail.com>
2020-10-03 09:31:29 -0400

    feat: adds covidData function to app.js and helpful comments

--- App.js
+++ App.js
@@ -6,29 +6,49 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Dimensions, StyleSheet, View } from 'react-native'
 import CovidMap from './components/Map'
+import rawCovidData from './assets/data/WHO-COVID-19-global-data.json'
+import rollingAverage from './assets/RollingAverage'
+import * as d3 from 'd3'
 
-class App extends Component {
-  state = {
-    count: 0
-  }
+const App = () => {
+  const [stat, setStat] = useState('avg_confirmed')
+  const [date, setDate] = useState('2020-10-01')
+  const windowSize = Dimensions.get('window')
 
-  onPress = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  render() {
-    const windowSize = Dimensions.get('window')
-    return (
-      <View style={styles.container}>
-        <CovidMap dimensions={windowSize} />
-      </View>
+  // The useMemo hook is useful here because we don't want all this calculation
+  // happening on every render, just when the values change.
+  const covidData = useMemo(() => {
+    // This will transform our data in to an array of objects (Object.keys returns an array)
+    const countryArray = Object.keys(rawCovidData).map((country) => ({
+      name: country,
+      data: rawCovidData[country]
+    }))
+    // For now, the windowSize is hardcoded (more work on dynamically changing it later)
+    const windowSize = 7
+    
+    const countriesWithAverage = countryArray.map((country) => ({
+      name: country.name,
+      data: [...rollingAverage(country.data, windowSize)]
+    }))
+    // This one will narrow down our list of countries to one _above_ a specified amount of
+    // daily cases (here set to 10 per day). The filter function will use findIndex to 
+    // determine if anything matches our criteria (d[stat] >= 10) and return the index or
+    // a negative 1 to show it wasn't found. We don't want anything that comes back `-1`
+    const filteredCountries = countriesWithAverage.filter(
+      (country) => country.data.findIndex((d, _) => d[stat] >= 10) != -1
     )
-  }
+
+    return filteredCountries
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <CovidMap dimensions={windowSize} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
