3e961a2eb1b3d044be350f04fd10c2cb4671bab1
Michael Maurer <maumike87@gmail.com>
2020-10-12 23:30:44 -0400

    feat: dataRestructure now converts from CSV

--- assets/dataRestructure.js
+++ assets/dataRestructure.js
@@ -1,20 +1,32 @@
-// The structure of the data from WHO's website changed, so this script will change it to something
-// this app expects. Run from root folder.
+// This node script will convert CSV to JSON, restructure the data, and remove anything before June.
+// Run from root folder.
 const fs = require('fs')
-const rawData = require('./data/WHO-COVID-19-global-data.json')
+const CSVToJSON = require('csvtojson')
 
-const restructureData = (rawData) => {
+const restructureData = (data) => {
   const newObj = {}
-  rawData.forEach((countryObj) => {
-    if (newObj[countryObj.Country]) {
-      newObj[countryObj.Country].push(countryObj)
-    } else newObj[countryObj.Country] = [countryObj]
+  data.forEach((countryObj) => {
+    if (++countryObj.Date_reported.split('-')[1] > 6) {
+      if (newObj[countryObj.Country]) {
+        newObj[countryObj.Country].push(countryObj)
+      } else newObj[countryObj.Country] = [countryObj]
+    }
   })
   return newObj
 }
 
-const restructured = JSON.stringify(restructureData(rawData))
+;(async () => {
+  try {
+    const rawDataJSON = await CSVToJSON().fromFile(
+      './assets/data/WHO-COVID-19-global-data.csv'
+    )
 
-fs.writeFile('./assets/data/rawData.json', restructured, (err) => {
-  if (err) throw err
-})
+    const restructured = JSON.stringify(restructureData(rawDataJSON))
+
+    fs.writeFile('./assets/data/rawData.json', restructured, (err) => {
+      if (err) throw err
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})()
